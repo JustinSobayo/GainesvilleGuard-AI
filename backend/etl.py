ed0daa20566b43c6a341acc45a6a71884a88fa36
justins <ksobayo0126@gmail.com>
2025-12-25 19:10:51 -0500

    fix: add dotenv loading to etl.py for environment variables

--- backend/etl.py
+++ backend/etl.py
@@ -3,10 +3,13 @@ from neo4j import GraphDatabase
 import os
 import datetime
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # --- CONFIGURATION ---
-NEO4J_URI = "bolt://localhost:7687"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "password"  # VALIDATE: User must update this!
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 class CrimeKnowledgeGraph:
     def __init__(self, uri, user, password):
